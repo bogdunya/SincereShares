a65b655cf2297ef8578eaf7e1bb586b7ae86315b
den1s0v <hello-lumen@ya.ru>
2023-04-19 19:44:54 +0300

    Задан язык - русский, и часовой пояс

--- website/sincereshares/sincereshares/settings.py
+++ website/sincereshares/sincereshares/settings.py
@@ -113,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+3'
 
 USE_I18N = True
 
