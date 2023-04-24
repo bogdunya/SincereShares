b5f236bcd1a799494eba93de9f9f7d7ad29014d3
Nadezhda Kosyura <nadejdakna2002@gmail.com>
2023-04-24 16:14:32 +0300

    Созданы таблицы share, price

--- website/sincereshares/db/models.py
+++ website/sincereshares/db/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Share(models.Model):
+    ticker = models.CharField(max_length=40)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+
+
+class Price(models.Model):
+    share = models.ForeignKey(Share, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    price = models.FloatField()
+    volume = models.FloatField()
+    change = models.FloatField()
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    currency = models.CharField(max_length=10)
+
--- website/sincereshares/sincereshares/settings.py
+++ website/sincereshares/sincereshares/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'moexplot',
+    'db',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC+3'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
